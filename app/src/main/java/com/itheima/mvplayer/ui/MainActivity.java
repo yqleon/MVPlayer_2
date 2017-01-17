93dd042dce61925f16b2ab8728d476ea510ee9f4
yqleon <1468541689@qq.com>
2017-01-17 16:12:10 +0800

    首次提交

--- app/src/main/java/com/itheima/mvplayer/ui/MainActivity.java
+++ app/src/main/java/com/itheima/mvplayer/ui/MainActivity.java
@@ -3,13 +3,13 @@ package com.itheima.mvplayer.ui;
 import com.itheima.mvplayer.R;
 
 public class MainActivity extends BaseActivity {
-
-    //继承的AppCompatActivity改为BaseActivity,
+    //继承AppCompatActivity改为继承BaseActivity,
     /*@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }*/
+    //给父类onCreat方法返回布局地址
     @Override
     public int getLayoutResId() {
         return R.layout.activity_main;
